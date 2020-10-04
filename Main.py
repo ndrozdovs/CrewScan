975d91b0aebe113d6b49be38ca1f5e1f5728680d
Nikita Drozdovs <ndrozdovs@icloud.com>
2020-10-03 17:41:13 -0700

    New stuff

--- Main.py
+++ Main.py
@@ -47,6 +47,7 @@ class BeginWindow(Screen):
         self.welcome = Clock.schedule_interval(partial(welcome, self, self.string_1), 1)
         disable_opacity(self.string_1, self.string_2, self.image_1)
         Clock.schedule_once(partial(animate, self.string_2,self.image_1), timeout=0.5)
+        Clock.schedule_once(partial(reset_globals), 0.5)
 
     # Execute before leaving the screen    
     def on_pre_leave(self):
@@ -179,9 +180,11 @@ class TemperatureWindow(Screen):
         
     def on_pre_leave(self):
         global PROGRESS_COUNTER
-        global DIST_COUNTER
         global TIMEOUT_COUNTER
+        global CLOSE_COUNTER
         PROGRESS_COUNTER = 0
+        TIMEOUT_COUNTER = 0
+        CLOSE_COUNTER = 0
         self.ids.progress.value = PROGRESS_COUNTER # Update the progress bar counter to 0 when leaving the screen
         self.event.cancel()
         self.timeout.cancel()
@@ -190,7 +193,6 @@ class TemperatureWindow(Screen):
     # Measure the persons temperature for 3 seconds and average it over time      
     def measure_temp(self, dt):
         global SAMPLE_COUNTER
-        global DIST_COUNTER
         global TEMPERATURE
         global TEMP_COUNTER
         global PROGRESS_COUNTER
@@ -198,6 +200,7 @@ class TemperatureWindow(Screen):
         global ERROR_CODE
         global POP_ACTIVE
         global TIMEOUT_COUNTER
+        global CLOSE_COUNTER
         
         if POP_ACTIVE == 1:
             start_time = time.time() 
@@ -227,19 +230,23 @@ class TemperatureWindow(Screen):
         
         distance = ReadDistance(17)    
         
-        if distance < 4:
-            disable_opacity(self.string_1, self.string_2, self.string_3, self.string_4, self.string_5, self.string_6, self.string_7)
-            enable_opacity(self.string_8)
+        if distance < 3.6:
+            CLOSE_COUNTER += 1
+            if CLOSE_COUNTER >= 5:
+                disable_opacity(self.string_1, self.string_2, self.string_3, self.string_4, self.string_5, self.string_6, self.string_7)
+                enable_opacity(self.string_8)
         # If distance read from the sensor is less than 10 centimeters
         elif distance < 10 and SAMPLE_COUNTER < 30 and TIMEOUT_COUNTER > 0:
+            CLOSE_COUNTER = 0
             disable_opacity(self.string_1, self.string_2, self.string_3, self.string_4, self.string_8)
             enable_opacity(self.string_5, self.string_6, self.string_7)
             SAMPLE_COUNTER += 1
             PROGRESS_COUNTER += 0.8333  # Increment by 3.33 because progress bar is up to 100 and we execute this for 1 second
             self.ids.progress.value = PROGRESS_COUNTER  # Update the progress bar
         elif distance < 10 and SAMPLE_COUNTER >= 30:
+            CLOSE_COUNTER = 0
             if distance > 3:
-                voltage = chan.voltage + 0.061                                                           # Add voltage for proper calibaration
+                voltage = chan.voltage + 0.06                                                           # Add voltage for proper calibaration
                 test_temperature = (2705.06 + ((-7670.457 - 2705.061) / (1 + (voltage / (3.135016*(10**-8)))**0.0595245))) + (0.1*(distance - 4.5))  # Convert voltage to degrees Celsius            
                 # If temperature is valid, count it as one of the data points
                 if test_temperature > 20 and test_temperature < 45:
@@ -253,10 +260,11 @@ class TemperatureWindow(Screen):
                             self.manager.current = "good_temp"                                                            # If temperature is below 38 degrees, go to good temperature window and print the pass
                         else:
                             self.manager.current = "fail"                                                                 # If temperature is above 38 degrees, go to fail screen
-        # If distance is less than 10 centimeters -> reset variables and progress bar, and start measuring again
+        # If distance is more than 10 centimeters -> reset variables and progress bar, and start measuring again
         elif TIMEOUT_COUNTER > 0:
             disable_opacity(self.string_5, self.string_6, self.string_7, self.string_8)
             enable_opacity(self.string_1, self.string_2, self.string_3, self.string_4)
+            CLOSE_COUNTER = 0
             SAMPLE_COUNTER = 0
             TEMPERATURE = 0
             PROGRESS_COUNTER = 0
@@ -301,7 +309,7 @@ class GoodTempWindow(Screen):
         d.text((20,50), str(dt_string), fill=(0,0,0), font=mono_14)
         d.text((150,88), "SetTek", fill=(0,0,0), font=mono_14) 
         img.save('pil_text.png')                                                                    # Save the image file
-        #os.system("brother_ql -b pyusb -m QL-700 -p usb://0x04f9:0x2042 print -l 62 pil_text.png")  # Send a console print command
+        os.system("brother_ql -b pyusb -m QL-700 -p usb://0x04f9:0x2042 print -l 62 pil_text.png")  # Send a console print command
 
 # Not currently used    
 class BadTempWindow(Screen):
@@ -353,13 +361,33 @@ class WindowManager(ScreenManager):
 # Used for adding text and progress bar for the pop up window in .kv file  
 class Popups(FloatLayout): 
     pass
-    
+
+
+def reset_globals(dt):
+    global TEMPERATURE
+    global SAMPLE_COUNTER
+    global TEMP_COUNTER
+    global PROGRESS_COUNTER
+    global TIMEOUT_COUNTER
+    TEMPERATURE = 0       # Global variable to track temperature from the sensor
+    SAMPLE_COUNTER = 0    # Global variable to track amount of samples taken for distance and temperature
+    TEMP_COUNTER = 0      # Global variable to track the amount of times we measured the temperature
+    PROGRESS_COUNTER = 0  # Global variable to track and output progress of the progress bar
+    TIMEOUT_COUNTER = 0   # Global variable to track the timeout of each screen
+        
     
 def welcome(instance, string, dt):
-    if ReadDistance(17) < 60:
-        Animation(opacity=1, duration=0.5).start(string)
-    else: 
-        Animation(opacity=0, duration=0.5).start(string)
+    for i in range(3):
+        print(i)
+        distance = ReadDistance(17)
+        time.sleep(.01)
+        if i is 2:
+            print("HELLO")
+            Animation(opacity=1, duration=0.5).start(string)
+        elif distance > 60: 
+            Animation(opacity=0, duration=0.5).start(string)
+            print("BREAK")
+            break
 
 
 # A function for checking if any of the pedals have been pressed
@@ -601,7 +629,7 @@ GPIO.setup(RIGHT_PEDAL,GPIO.IN,pull_up_down=GPIO.PUD_UP)   # Setup Right pedal a
 GPIO.setup(LEFT_PEDAL,GPIO.IN,pull_up_down=GPIO.PUD_UP)    # Setup Left pedal as GPIO
 GPIO.setup(MIDDLE_PEDAL,GPIO.IN,pull_up_down=GPIO.PUD_UP)  # Setup Middle pedal as GPIO
 
-Window.size = (480, 800)                                   # Set screen size to 480x800, if we don't want to use fullscreen (should be fullscreen is final version)
+#Window.size = (480, 800)                                   # Set screen size to 480x800, if we don't want to use fullscreen (should be fullscreen is final version)
 
 kv = Builder.load_file("my.kv")                            # Load .kv file for GUI interface
 
@@ -621,5 +649,5 @@ class MyApp(App):
 
 
 if __name__ == "__main__":
-    #Window.fullscreen = 'auto'  
+    Window.fullscreen = 'auto'  
     MyApp().run()
